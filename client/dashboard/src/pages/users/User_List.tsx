e981b1e4a7ba8d504d1f1950dcaa9add426d2cd2
mariam-elsarag <mariamelsarag44@gmail.com>
2025-09-12 11:54:27 +0200

    feat: user page

---
+++ client/dashboard/src/pages/users/User_List.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import Page_Wraper from "../../components/layout/page_wraper/Page_Wraper";
+import Table_Layout from "../../components/shared/table/Table_Layout";
+import type { UserType } from "../../context/Auth_Context.types";
+import { API } from "../../services/apiUrl";
+import Avatar from "../../components/shared/avatar/Avatar";
+import { userStatusBadge } from "../../common/lists/Badges_List";
+import Badge from "../../components/shared/badge/Badge";
+
+const tapList = [
+  {
+    name: "users",
+    value: "user",
+    fieldName: "role",
+    default: true,
+  },
+  {
+    name: "admin",
+    value: "admin",
+    fieldName: "admin",
+  },
+];
+const User_List = () => {
+  const [filter, setFilter] = useState("user");
+  const columns = [
+    {
+      header: "name",
+      field: "user",
+      body: (item) => (
+        <div className="truncate flex items-center gap-6">
+          <Avatar avatar={item?.user?.avatar} fullName={item?.user?.fullName} />
+          <span>{item?.user?.fullName}</span>
+        </div>
+      ),
+    },
+    {
+      header: "email",
+      field: "email",
+      body: (item) => item?.email ?? "-",
+    },
+    {
+      header: "phone",
+      field: "phone",
+      body: (item) =>
+        item?.phone ? <span dir="ltr">{item?.phone}</span> : "-",
+    },
+    filter === "user" && {
+      header: "address",
+      field: "address",
+      body: (item) => item?.address ?? "-",
+    },
+    filter === "user" && {
+      header: "status",
+      field: "status",
+      body: (item) => {
+        const { text, type } = userStatusBadge(item?.status);
+        return <Badge text={text} type={type} />;
+      },
+    },
+  ];
+  return (
+    <Page_Wraper label="users">
+      <Table_Layout<UserType>
+        title={filter === "user" ? "users" : "admin"}
+        hasPagination={true}
+        columns={columns}
+        emptyText={filter === "user" ? "no_users_yet" : "no_admin_yet"}
+        endpoint={API.users.list}
+        search_placeholder={filter === "user" ? "search_user" : "search_admin"}
+        hasTap={true}
+        tapList={tapList}
+        tapType="click"
+        onClick={(val) => {
+          setFilter(val);
+        }}
+      />
+    </Page_Wraper>
+  );
+};
+
+export default User_List;
