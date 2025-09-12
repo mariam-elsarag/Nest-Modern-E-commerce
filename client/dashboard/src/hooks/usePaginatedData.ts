167890bef702099b44d3ead5d6fad1b8ed20d3f3
mariam-elsarag <mariamelsarag44@gmail.com>
2025-09-12 08:50:44 +0200

    fix: error while import axios

--- client/dashboard/src/hooks/usePaginatedData.ts
+++ client/dashboard/src/hooks/usePaginatedData.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import axiosInstance from "~/services/axiosInstance";
+import axiosInstance from "../services/axiosInstance";
 
 type UsePaginatedDataProps = {
   endpoint: string;
@@ -16,6 +16,7 @@ function usePaginatedData<T = { id: number }>({
   const [pages, setPages] = useState<number>(1);
   const [error, setError] = useState<Error | unknown>();
   const [loading, setLoading] = useState(false);
+  const [searchLoader, setSearchLoader] = useState(false);
   const [query, setQuery] = useState(queryDefault);
   const [refetchData, setRefetchData] = useState();
   const [hasMore, setHasMore] = useState<boolean>(false);
@@ -36,31 +37,34 @@ function usePaginatedData<T = { id: number }>({
       }
 
       const results = currentData?.results || [];
-      if (results.length > 0) {
-        if (Array.isArray(results)) {
-          if (type === "pages") {
-            setData(results);
-          } else {
-            setData((prevData) => {
-              const allData =
-                currentPage === 1 ? [...results] : [...prevData, ...results];
-              const uniqueData = allData.reduce<T[]>((acc, item) => {
-                if (!acc.some((existing) => existing.id === item.id)) {
-                  acc.push(item);
-                }
-                return acc;
-              }, []);
-              return uniqueData;
-            });
-          }
-        }
-      } else {
-        setData(currentData);
-      }
+      // if (results.length > 0) {
+      //   if (Array.isArray(results)) {
+      //     if (type === "pages") {
+      //       setData(results);
+      //     } else {
+      //       setData((prevData) => {
+      //         const allData =
+      //           currentPage === 1 ? [...results] : [...prevData, ...results];
+      //         const uniqueData = allData.reduce<T[]>((acc, item) => {
+      //           if (!acc.some((existing) => existing.id === item.id)) {
+      //             acc.push(item);
+      //           }
+      //           return acc;
+      //         }, []);
+      //         return uniqueData;
+      //       });
+      //     }
+      //   }
+      // } else {
+      //   setData(currentData);
+      // }
     } catch (err) {
       setError(err);
     } finally {
       setLoading(false);
+      if (searchLoader) {
+        setSearchLoader(false);
+      }
     }
   };
 
@@ -104,6 +108,8 @@ function usePaginatedData<T = { id: number }>({
     setQuery,
     handlePagination,
     handleScroll,
+    searchLoader,
+    setSearchLoader,
   };
 }
 export default usePaginatedData;
