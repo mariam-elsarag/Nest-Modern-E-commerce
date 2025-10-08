107ad36b7f711108e77349ed9e5bbf56ba361241
mariam-elsarag <mariamelsarag44@gmail.com>
2025-10-08 21:55:33 +0200

    Cloudinary setup

--- client/dashboard/src/pages/settings/Setting.tsx
+++ client/dashboard/src/pages/settings/Setting.tsx
@@ -5,9 +5,14 @@ import type { FormListItemType } from "../../components/shared/form_builder/Form
 import { useTranslation } from "react-i18next";
 import { useForm } from "react-hook-form";
 import Form_Builder from "../../components/shared/form_builder/Form_Builder";
-import { emailRegex } from "../../common/constant/validator";
+import { emailRegex, taxRatePattern } from "../../common/constant/validator";
 import Button from "../../components/shared/button/Button";
 import View_Colors from "../../components/shared/form_builder/View_Colors";
+import { handleError } from "../../common/utils/handleError";
+import axiosInstance from "../../services/axiosInstance";
+import { API } from "../../services/apiUrl";
+import { toast } from "react-toastify";
+import useGetData from "../../hooks/useGetData";
 
 const Setting = () => {
   const { t } = useTranslation();
@@ -27,10 +32,13 @@ const Setting = () => {
   } = useForm({
     defaultValues: {
       monthlyOrderGoal: "",
-      supportEmail: "",
+      taxRate: "",
     },
     mode: "onChange",
   });
+
+  const { data: settingData } = useGetData(API.settting.setting, setValue);
+
   const formList: FormListItemType[] = [
     {
       id: "1",
@@ -58,15 +66,57 @@ const Setting = () => {
       label: "tax_rate",
       placeholder: "tax_rate",
       fieldName: "taxRate",
+      validator: {
+        pattern: {
+          value: taxRatePattern,
+          message: "invalid_tax_rate",
+        },
+      },
+      onKeyDown: (e) => {
+        const allowedKeys = [
+          "Backspace",
+          "Tab",
+          "ArrowLeft",
+          "ArrowRight",
+          "Delete",
+          "Home",
+          "End",
+          ".",
+        ];
+
+        if (!/^[0-9]$/.test(e.key) && !allowedKeys.includes(e.key)) {
+          e.preventDefault();
+        }
+
+        if (e.key === "." && e.currentTarget.value.includes(".")) {
+          e.preventDefault();
+        }
+      },
     },
   ];
-
+  //__________________ function ____________
+  const onSubmit = async (data) => {
+    try {
+      setLoading(true);
+      const response = await axiosInstance.patch(API.settting.setting, data);
+      if (response.status === 200) {
+        toast.success(t("successfully_update_settings"));
+      }
+    } catch (err) {
+      handleError(err, t, setError);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <Page_Wraper containerClassName="layer shadow_sm min-h-[75vh] py-6">
       {" "}
       <Page_Title title="main_setting" />
       <section className="px-4 grid gap-6 h-full">
-        <form className=" max-w-[600px] grid gap-10  ">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className=" max-w-[600px] grid gap-10  "
+        >
           <fieldset className="grid    gap-6">
             <Form_Builder
               formList={formList}
